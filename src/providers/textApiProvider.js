e075d15d6b89dcd0b5e3c362a35ecf9727237d0d
NicolasDESPRIET <nicolas.despriet@isen.yncrea.fr>
2021-02-24 15:18:30 +0100

    comment add

---
+++ src/providers/textApiProvider.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+const baseUrl = "https://loripsum.net/api";
+
+exports.getRandomText = async () => {
+    let response = await axios.get(baseUrl + "/plaintext", {responseType : "text"});
+    return response.data;
+}
+
+//exports.getRandomText = () => {
+//    return new Promise((resolve, reject) => {
+//        axios.get(baseUrl+"/plaintext", {
+//            responseType: "text"
+//        }).then((Response) => {
+//            resolve(response.data)
+//        }).catch((error) => {
+//            console.log(error);
+//            reject(false);
+//        })
+//    })
+//}
